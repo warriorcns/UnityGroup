32fe75239717db1bae0153a8a067a1b702672401
paul474 <paul474@wp.pl>
2013-11-27 15:16:59 +0100

    Poprawiont teren, usunięte śmieci

--- Teren/New Unity Project 2/Assets/Scripts/CarControleScript.js
+++ Teren/New Unity Project 2/Assets/Scripts/CarControleScript.js
@@ -10,28 +10,23 @@ var wheelFRTrans : Transform;
 var wheelRLTrans : Transform;
 var wheelRRTrans : Transform;
 
-var minSpeed : float = 0;  //minimalna szybkość
+var minSpeed : float = -50;  //minimalna szybkość
 var maxSpeed : float = 250;  //maksymalna szybkość
 var speed : float;    //szybkość
-var steerSpeed = 20;
-var steer_max = 20;
 
 private var friction : float;  //tarcie
 private var forwardFrict : float;
 private var slipFriction : float;
 private var slipForwardFrict : float;
-private var steer = 0;
 
 var lowSpeedSteerAngle : float = 10;
 var highSpeedSteerAngle : float = 1;
 var maxReverseSpeed : float = 50;
 var decelleration : float = 30;   //zmniejszenie prędkości
 var maxTorque : float = 50;  //maksymalny moment obrotowy
-var breaking = -100;      //hamowanie
+var breaking = 50;      //hamowanie
 var braked : boolean = false;   //czy hamuje
-var maxBrake : float = -50;    //maksymalna wartość hamowania
-var AntiRoll = 5000.0;
-public var RotationValue : float = 0.0;
+var maxBrake : float = 100;    //maksymalna wartość hamowania
 
 var wfc : WheelFrictionCurve;
 
@@ -40,8 +35,6 @@ var centerOfMass : Transform;
 function Start(){
 	//rigidbody.AddForce(-Physics.gravity);	
 	rigidbody.centerOfMass = centerOfMass.localPosition;
-	
-	//var euler = wheel.localEulerAngles;
 	
 	//ustawianie tarcia bocznego i przedniego
 	forwardFrict = wheelRR.forwardFriction.stiffness;   
@@ -107,54 +100,10 @@ function Update(){
 	Rotate(wheelFRTrans, wheelFR);
 	Rotate(wheelRLTrans, wheelRL);
 	Rotate(wheelRRTrans, wheelRR);
-
 	
-	if ( steer == 0 && wheelFL.steerAngle != 0) {
- 		if (Mathf.Abs(wheelFL.steerAngle) <= (steerSpeed * Time.deltaTime)) {
-				wheelFL.steerAngle = 0;
-			} else if (wheelFL.steerAngle > 0) {
-				wheelFL.steerAngle = wheelFL.steerAngle - (steerSpeed * Time.deltaTime);
-		 	} else {
-				wheelFL.steerAngle = wheelFL.steerAngle + (steerSpeed * Time.deltaTime);
-		 	}
-	} 
-   else {
-		 wheelFL.steerAngle = wheelFL.steerAngle + (steer * steerSpeed * Time.deltaTime);
-		  if (wheelFL.steerAngle > steer_max) { wheelFL.steerAngle = steer_max; }
-		 if (wheelFL.steerAngle < -1 * steer_max) { wheelFL.steerAngle = -1 * steer_max; }
-  	}  	
-  	wheelFR.steerAngle = wheelFL.steerAngle;
 	//obrót koła przy skręcaniu
-	wheelFLTrans.localEulerAngles.z = wheelFL.steerAngle;   
-	wheelFRTrans.localEulerAngles.z = wheelFR.steerAngle; //- wheelFRTrans.localEulerAngles.z;
-	
-	// update actual wheel localEulerAngles:
-	//wheelFLTrans.localEulerAngles.z = wheelFLTrans.localEulerAngles.z;
-	//wheelFRTrans.localEulerAngles.z = wheelFRTrans.localEulerAngles.z;
-	
-	//wheelFL.transform.rotation = wheelFL.transform.rotation * Quaternion.Euler( RotationValue, wheelFL.steerAngle, 0);
-	//RotationValue += wheelFL.rpm * ( 360/60 ) * Time.deltaTime;
-	
-	var hit : WheelHit;
-    var travelL = 1.0;
-    var travelR = 1.0;
-
-    var groundedL = wheelFL.GetGroundHit(hit);
-    if (groundedL)
-        travelL = (-wheelFL.transform.InverseTransformPoint(hit.point).y - wheelFL.radius) / wheelFL.suspensionDistance;
-
-    var groundedR = wheelFR.GetGroundHit(hit);
-    if (groundedR)
-        travelR = (-wheelFR.transform.InverseTransformPoint(hit.point).y - wheelFR.radius) / wheelFR.suspensionDistance;
-
-    var antiRollForce = (travelL - travelR) * AntiRoll;
-
-    if (groundedL)
-        rigidbody.AddForceAtPosition(wheelFL.transform.up * -antiRollForce,
-               wheelFL.transform.position);	
-    if (groundedR)
-        rigidbody.AddForceAtPosition(wheelFR.transform.up * antiRollForce,
-               wheelFR.transform.position);	
+	wheelFLTrans.localEulerAngles.y = -wheelFL.steerAngle - wheelFLTrans.localEulerAngles.z;   
+	wheelFRTrans.localEulerAngles.y = -wheelFR.steerAngle - wheelFRTrans.localEulerAngles.z;
 }
 
 //Obrót danego koła
