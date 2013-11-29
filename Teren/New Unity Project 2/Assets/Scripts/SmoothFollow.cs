78e6da70e65daad4797e0667bc12e997e880cc56
kciesiel <k.d.ciesielski@gmail.com>
2013-11-29 23:26:31 +0100

    Wrzucony samochód do nowego terenu
    Wrzucone auto choć jeszcze trochę do poprawy ;)

---
+++ Teren/New Unity Project 2/Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour 
+{	
+	public Transform target;
+	public float distance = 10.0f;
+	public float height = 5.0f;
+	public float heightDamping = 2.0f;
+	public float rotationDamping = 3.0f;
+	
+	private float wantedRotationAngle, wantedHeight;
+	private float currentRotationAngle, currentHeight;
+	
+	void LateUpdate (){
+		if (!target)
+			return;
+
+		wantedRotationAngle = target.eulerAngles.y;
+		wantedHeight = target.position.y + height;
+		
+		currentRotationAngle = transform.eulerAngles.y;
+		currentHeight = transform.position.y;
+
+		currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+		currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+		Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+		transform.position = target.position;
+		transform.position -= currentRotation * Vector3.forward * distance;
+
+		Vector3 newPos = transform.position;
+		newPos.y = currentHeight;
+		transform.position = newPos;
+
+		transform.LookAt (target);
+	}
+}
